b5dc880b7fb573e1c60712a1799329ee0efb694a
jakobcornell <jakobcornell@gmail.com>
2014-01-11 17:00:41 -0600

    improve paddle appearance

--- src/com/jakobcornell/compsci/brickout/Paddle.java
+++ src/com/jakobcornell/compsci/brickout/Paddle.java
@@ -1,24 +1,25 @@
 package com.jakobcornell.compsci.brickout;
 
-import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 import java.util.Collection;
 
 public class Paddle {
-   public double x;
-   public double xv;
-   public int size;
-   protected boolean guns;
-   public Ball loadedBall;
-   
-   public Ball launch() {
-      return new Ball();
-   }
-   
-   public Collection<Ball> shoot() {
-      return null;
-   }
-   
-   public void paint(Graphics g) {
-      g.drawRect((int) (x-size/2), 10, size, 10);
-   }
+  public double x;
+  public double xv;
+  public int size = 5;
+  protected boolean guns;
+  public Ball loadedBall;
+
+  public Ball launch() {
+    return new Ball();
+  }
+
+  public Collection<Ball> shoot() {
+    return null;
+  }
+
+  public void paint(Graphics2D g) {
+    g.draw(new Rectangle2D.Double(x-size/2, 1, size, 1));
+  }
 }
